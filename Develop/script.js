7a89be01e5b2d71e53155a70ef733252019383c3
Carlos Macias <cimm21187@gmail.com>
2023-07-25 22:14:42 -0400

    Commit defining the sets of characters and the lenght of the password

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,23 @@
 // Assignment code here
 
+// Function to generate a random password
+function generatePassword() {
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numeric = "0123456789";
+  var special = "!@#$%^&*()_-+=[]{}|;:,.<>?";
+
+  // Ask user for password length
+  var passwordLength = prompt("Enter the desired password length (between 8 and 128 characters):");
+
+  // Check if the user entered a valid number
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a valid number between 8 and 128.");
+    return "";
+  }
+  
+}
+  
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
