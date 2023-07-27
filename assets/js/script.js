16804c01db98c92165145f5c97f8f234912846e9
Carlos Macias <cimm21187@gmail.com>
2023-07-26 22:41:25 -0400

    Adjustment to the characters variables adding a split

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@
 
 // Function to generate a random password
 function generatePassword() {
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numeric = "0123456789";
-  var special = "!@#$%^&*()_-+=[]{}|;:,.<>?";
+  var lowercase = "abcdefghijklmnopqrstuvwxyz".split(',');
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(',');
+  var numeric = "0123456789".split(',');
+  var special = "!@#$%^&*()_-+=[]{}|;:,.<>?".split(',');
 
   // Ask user for password length
   var passwordLength = prompt("Enter the desired password length (between 8 and 128 characters):");
